7fe486d20764f46f90b4b3f6417ec73b24a1f5a2
Andre <andrenicolassv@gmail.com>
2026-06-29 22:06:14 -0300

    funcoes

--- funcoes.py
+++ funcoes.py
@@ -1,8 +1,15 @@
 import os
 import json
 from time import sleep
+
+
+"""na função cadastrar, é coletado a informação nome, com isso verifica se esse nome já existe no arquivo 'arquivo.txt' 
+   se não tem esse nome nesse arquivo, então o programa segui adiante, fazendo um arquivo em json para os palpites do 
+   apostador, categorizando eles em 6 partidas para cada grupo, 16 partidas paras os 32 finais, 8 partidas para as   
+   oitavas, 4 partidas para as quartas, 2 para as semis, uma para final e adicionando o nome do apostador ao arquivo 
+   'apostadores.txt' """
 def cadastrar ():
-    grupos = {
+    gruposLetras = {
     1 : 'A', 2: 'B', 3 : 'C', 4 : 'D', 5: 'E', 6: 'F', 7: 'G', 8 : 'H', 9 : 'I', 10 : 'J', 11 : 'K', 12: 'L' 
 
     }
@@ -21,11 +28,11 @@ def cadastrar ():
                     if i % 6 == 0:
                         contador += 1
                 dados = {
-                "id": i,
+                "id": i + 1,
                 "fase": 1,
-                "grupo": grupos[contador],
-                "selecao1": "",
-                "selecao2": "",
+                "grupo": gruposLetras[contador],
+                "selecao1": " ",
+                "selecao2": " ",
                 "gols1": -1, 
                 "gols2": -1
                 }
@@ -58,8 +65,8 @@ def cadastrar ():
                 dados = {
                 "id": id ,
                 "fase": fase,
-                "selecao1": "",
-                "selecao2": "",
+                "selecao1": " ",
+                "selecao2": " ",
                 "gols1": -1, 
                 "gols2": -1
                 }
@@ -72,36 +79,72 @@ def cadastrar ():
                 sleep (2)
 def limpar():
     os.system ('cls')
-def registrar(escolha2):
+"""na função registrar, existem dois modos, interativo, na qual o usuário terá um menu na qual escolher entre 3 opções para seguir, listar todoso os jogos, listar apenas os sem 
+   palpites ou cadastrar ou alterar o placar de um jogo, dentro dessa última funcionalidade o usuário deverá digitar o id do jogo que quer 
+   alterar ou cadastrar, o outro modo é o em lote... (volte aqui para adicionar mais)"""
+
+
+def registrar(escolha2, partidas):
     lista = []
+    jogos = []
+    status = True
+    nome = input('Digite o seu nome:\n')
+    with open (f'palpites_{nome}.json', 'r', encoding = 'utf-8') as arquivo:
+                    leitura = json.load(arquivo)
+    if  leitura[71]['selecao1'].strip() is None and leitura[71]['selecao2'].strip() is None:
+        try:
+            for i in leitura:
+                status = True
+                for j in partidas:
+                    if not status:
+                        break
+                    if (i.get('id') == j.get('id')):
+                        status = False
+                        i.update({'selecao1' : j.get('selecao1'), 'selecao2' : j.get('selecao2')})
+            with open(f'palpites_{nome}.json', 'w', encoding = 'utf-8') as arquivo:
+                json.dump(leitura, arquivo, indent = 4)
+        except TypeError:
+            print('ops! voce esqueceu de carregar as selecoes, volte aqui mais tarde ;)')
+            sleep(2)
+            return
+    
+
+        
+        
     if escolha2 == 1:
-        nome = input('Digite o seu nome:\n')
-        escolha3 = int (input('''--------------------------------------------
+
+        while True:
+            limpar()
+            escolha3 = int (input('''--------------------------------------------
 [1] listar todos os jogos do bolão
 [2] listar apenas jogos sem palpite
 [3] cadastrar ou alterar o placar de um jogo
- '''))
-        with open (f'palpites_{nome}.json', 'r', encoding = 'utf-8') as arquivo:
-                    leitura = json.load(arquivo)
-        match escolha3:
-            case 1:
-                for i in leitura:
-                    print(f'{i}\n')
-                espera = input ()
-            case 2:
-                for i in leitura:
-                    if i.get('gols1') == -1 and i.get('gols1') == -1:
-                        print (f'{i}\n')
-                espera = input()
-            case 3:
-                escolha4 = int(input('--------------------------------------------\nqual id do jogo que desejar cadastrar ou alterar o placar:'))
-                for i in leitura:
-                    if i.get('id') == escolha4:
-                        print(i)
-                gols1 = int(input('digite a quantidade de gols para primeira selecao:'))
-                gols2 = int(input('digite a quantidade de gols para segunda selecao:'))
-                for i in leitura:
-                    if i.get ('id') == escolha4:
-                        i.update({'gols1': gols1, 'gols2' : gols2})
-                with open (f'palpites_{nome}.json ', 'w', encoding = 'utf-8') as arquivo:
-                    json.dump(leitura, arquivo, indent = 4)
+[0] sair
+    '''))
+
+            match escolha3:
+                case 1:
+                    for i in leitura:
+                        print(f'{i}\n')
+                    espera = input ()
+                case 2:
+                    for i in leitura:
+                        if i.get('gols1') == -1 and i.get('gols1') == -1:
+                            print (f'{i}\n')
+                    espera = input()
+                case 3:
+                    escolha4 = int(input('--------------------------------------------\nqual id do jogo que desejar cadastrar ou alterar o placar:'))
+                    for i in leitura:
+                        if i.get('id') == escolha4:
+                            print(i)
+                    gols1 = int(input('digite a quantidade de gols para primeira selecao:'))
+                    gols2 = int(input('digite a quantidade de gols para segunda selecao:'))
+
+                    for i in leitura:
+                        if i.get ('id') == escolha4:
+                            i.update({'gols1': gols1, 'gols2' : gols2})
+
+                    with open (f'palpites_{nome}.json ', 'w', encoding = 'utf-8') as arquivo:
+                        json.dump(leitura, arquivo, indent = 4)
+                case 0 : 
+                    break
--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 from funcoes import cadastrar, limpar, registrar
+from carregarSelecoes import carregarSelecoes
+from time import sleep
+grupos = {}
+status = False
+partidas = None
+
+
 
 while True:
     limpar()
     print ('MENU'.center(30, '='))
     escolha = int(input ('''[1] cadastrar apostador
 [2] registrar palpite
+[3] carregar selecoes
 [0] sair
 '''))
-    if escolha == 1:
-        cadastrar()
-    elif escolha == 2:
-        limpar()
-        print(30 * '-')
-        escolha2 = int(input ("""[1] modo interativo
-[2] modo em lote
-"""))
-        registrar(escolha2)
-    if escolha == 0:
-        break
+    match escolha:
+
+        case 1:
+            cadastrar()
+
+        case 2:
+            limpar()
+            print(70 * '-')
+            
+            escolha2 = int(input ("""[1] modo interativo
+[2] modo em loteS
+    """))
+            registrar(escolha2, partidas)
+        case 3:
+            grupos, status, partidas = carregarSelecoes()
+            for i in grupos.items():
+                print(f'{i}\n')
+            espera = input()
+            print('Fase de grupos'.center(100, '='))
+            for i in partidas:
+                print(f'{i}\n')
+            espera = input()
+        case 0:
+            break 
