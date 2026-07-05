4c299c170f6b1319f6ed729deb1a456526176209
Maria Eduarda <alves.maria.es@gmail.com>
2026-07-05 14:49:30 -0300

    criacao de arquivo dos apostadores caso ele não exista

--- funções/cadastrar_apostador.py
+++ funções/cadastrar_apostador.py
@@ -10,30 +10,36 @@ from time import sleep
 def cadastrar ():
     gruposLetras = {1 : 'A', 2: 'B', 3 : 'C', 4 : 'D', 5: 'E', 6: 'F', 7: 'G', 8 : 'H', 9 : 'I', 10 : 'J', 11 : 'K', 12: 'L' }
     nome = input('''Digite o seu nome: ''')
-    with open ('./apostadores/apostadores.txt', 'r') as arquivo:
-        leitura = arquivo.read()
-        if nome in leitura:
-            print('Esse nome ja esta cadastrado!')
-            sleep (5)
-        else:
-            lista = []
-            contador = 0
-            for  i in range(72):
-                if i % 6 == 0:
-                    contador += 1
-                dados = {
-                "id": i + 1,
-                "fase": 1,
-                "grupo": gruposLetras[contador],
-                "selecao1": "",
-                "selecao2": "",
-                "gols1": -1, 
-                "gols2": -1
-                }
-                lista.append(dados)
-            with open ('./apostadores/apostadores.txt', 'a') as arquivo:
-                arquivo.write(f'{nome}\n')     
-            with open (f'./apostadores/palpites_{nome}.json ', 'w', encoding = 'utf-8') as arquivo:
-                json.dump(lista, arquivo, indent = 4)
-                print ('Cadastrado com sucesso!')
-                sleep (2)
+    try:
+        with open ('./apostadores/apostadores.txt', 'r') as arquivo:
+            leitura = arquivo.read()
+            if nome in leitura:
+                print('Esse nome ja esta cadastrado!')
+                sleep (5)
+            else:
+                lista = []
+                contador = 0
+                for  i in range(72):
+                    if i % 6 == 0:
+                        contador += 1
+                    dados = {
+                    "id": i + 1,
+                    "fase": 1,
+                    "grupo": gruposLetras[contador],
+                    "selecao1": "",
+                    "selecao2": "",
+                    "gols1": -1, 
+                    "gols2": -1
+                    }
+                    lista.append(dados)
+                with open ('./apostadores/apostadores.txt', 'a') as arquivo:
+                    arquivo.write(f'{nome}\n')     
+                with open (f'./apostadores/palpites_{nome}.json ', 'w', encoding = 'utf-8') as arquivo:
+                    json.dump(lista, arquivo, indent = 4)
+                    print ('Cadastrado com sucesso!')
+                    sleep (2)
+    except FileNotFoundError:
+        print("O arquivo não de apostadores existe. Criando um novo... Tente novamente.")
+        input("Aperte ENTER para continuar")
+        with open('./apostadores/apostadores.txt', 'w') as arquivo:
+            arquivo.write("")
